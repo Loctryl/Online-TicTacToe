723ddfa0582a2e8209a275abc93dc33e53016b68
FrCollart <fcollart@gaming.tech>
2023-11-13 12:57:51 +0100

    client google

---
+++ Server/server.cpp
@@ -0,0 +1,39 @@
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <iphlpapi.h>
+#include <stdio.h>
+
+#pragma comment(lib, "Ws2_32.lib")
+
+int main() {
+
+#define DEFAULT_PORT "3000"
+
+    WSADATA wsaData;
+    int iResult;
+
+    // Initialize Winsock
+    iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    if (iResult != 0) {
+        printf("WSAStartup failed: %d\n", iResult);
+        return 1;
+    }
+
+    struct addrinfo* result = NULL, * ptr = NULL, hints{};
+    SOCKET ListenSocket = INVALID_SOCKET;
+
+    ListenSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (ListenSocket == INVALID_SOCKET) {
+        printf("Error at socket(): %ld\n", WSAGetLastError());
+        freeaddrinfo(result);
+        WSACleanup();
+        return 1;
+    }
+
+    return 0;
+}
